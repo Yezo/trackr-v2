3df40c2af7a16a7e7c9690474dcc50515cc07bb9
Kev <sulpeum@gmail.com>
2023-07-27 16:47:02 -0400

    refactor: update mobile navigation

--- components/navbar/mobile-nav.tsx
+++ components/navbar/mobile-nav.tsx
@@ -1,6 +1,6 @@
 import ExternalLinks from "@/components/navbar/external-links"
 import Logo from "@/components/typography/Logo"
-import { Button } from "@/components/ui/button"
+
 import {
   Sheet,
   SheetContent,
@@ -9,11 +9,11 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import { HamburgerMenuIcon, PlusIcon } from "@radix-ui/react-icons"
+import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 
 export default function MobileNav() {
   return (
-    <div className="sm:hidden h-14 flex items-center container justify-between border-b dark:border-gray-800 border-gray-200">
+    <div className="sm:hidden h-16 flex items-center container justify-between border-b dark:border-gray-800 border-gray-200">
       <div className="flex items-center gap-2">
         <Sheet>
           <SheetTrigger className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9">
@@ -29,12 +29,9 @@ export default function MobileNav() {
             </SheetHeader>
           </SheetContent>
         </Sheet>
-        <Button size="icon" variant="outline">
-          <PlusIcon className="h-[1.2rem] w-[1.2rem]" />
-        </Button>
       </div>
 
-      <Logo>Trackr.</Logo>
+      <Logo />
 
       <ExternalLinks />
     </div>
