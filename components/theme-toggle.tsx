86d1e31a9f5205341eefbe695c3d01ff1522a9b5
Kev <sulpeum@gmail.com>
2023-07-27 15:29:53 -0400

    style: change button variant to outline

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -11,7 +11,7 @@ export function ModeToggle() {
 
   return (
     <Button
-      variant="ghost"
+      variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
