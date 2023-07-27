53b39e093a5e843f2d086988e5ab337036779fc2
Kev <sulpeum@gmail.com>
2023-07-27 16:47:18 -0400

    feat: add logo image to component

--- components/typography/Logo.tsx
+++ components/typography/Logo.tsx
@@ -1,13 +1,29 @@
 import Link from "next/link"
+import Image from "next/image"
 
-type Props = {
-  children: React.ReactNode
-}
-
-export default function Logo({ children }: Props) {
+export default function Logo() {
   return (
-    <Link href="/" className="text-lg font-semibold tracking-tighter transition-colors delay-100">
-      {children}
+    <Link
+      href="/"
+      className="text-lg font-bold tracking-tight transition-colors delay-100 sm:flex items-center gap-2 hidden"
+    >
+      <Image
+        src="/dark-logo.webp"
+        width={100}
+        height={100}
+        alt="Playground"
+        className="h-4 w-4 rounded-full ring-2 ring-black dark:block hidden"
+        priority
+      />
+      <Image
+        src="/light-logo.webp"
+        width={100}
+        height={100}
+        alt="Playground"
+        className="h-4 w-4 rounded-full ring-2 ring-black dark:hidden block "
+        priority
+      />
+      Trackr.
     </Link>
   )
 }
