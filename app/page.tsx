6d6f84d84576dbc9560f9b5e89cdba3bcec8bb17
Kev <sulpeum@gmail.com>
2023-07-27 16:47:31 -0400

    feat: add navigation bars

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,3 @@
-import { ModeToggle } from "@/components/theme-toggle"
-import { Button } from "@/components/ui/button"
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-      <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account and remove
-              your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-
-      <ModeToggle />
-    </div>
-  )
+  return <div className="container py-5">Content</div>
 }
