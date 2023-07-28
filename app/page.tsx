7cc41179391c3d82dd2f3f0679e99e1d295d805a
Kev <sulpeum@gmail.com>
2023-07-28 16:09:02 -0400

    feat: add buttons for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import AddJobForm from "@/components/forms/add-job"
+import CreateAccountForm from "@/components/forms/create-account"
+import SignInForm from "@/components/forms/sign-in"
+
 export default function Home() {
-  return <div className="container py-5">Content</div>
+  return (
+    <div className="container py-5 grid place-items-center gap-4">
+      <SignInForm />
+      <CreateAccountForm />
+      <AddJobForm />
+    </div>
+  )
 }
