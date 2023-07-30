0f3ab5c743fe77378110fa389c83295032666759
Kev <sulpeum@gmail.com>
2023-07-30 17:47:24 -0400

    feat: add redirect

--- app/(private_route)/layout.tsx
+++ app/(private_route)/layout.tsx
@@ -6,6 +6,7 @@ type Props = {
   children: React.ReactNode
 }
 export default async function Component({ children }: Props) {
+  //This layout is for routes that you want to PREVENT NON-USERS FROM SEEING and then get REDIRECTED
   const session = await getServerSession(authOptions)
   //If the user isn't logged in, then redirect to login page
   if (!session?.user) redirect("/login")
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFoundRedirect } from "@/components/not-found"
+
+export default function NotFound() {
+  return <NotFoundRedirect />
+}
