39a74a12134137d25245279716a86bccd59ca63b
Kev <sulpeum@gmail.com>
2023-08-09 17:39:43 -0400

    test: attempt live

--- app/(private_route)/dashboard/page.tsx
+++ app/(private_route)/dashboard/page.tsx
@@ -16,8 +16,8 @@ import { AddJobForm } from "@/components/forms/add-job"
 
 async function getData(id: string | undefined) {
   try {
-    // const data = await fetch(`${process.env.LIVE_URL}/api/job/${id}`, {
-    const data = await fetch(`${process.env.LOCAL_URL}/api/job/${id}`, {
+    const data = await fetch(`${process.env.LIVE_URL}/api/job/${id}`, {
+      // const data = await fetch(`${process.env.LOCAL_URL}/api/job/${id}`, {
       cache: "no-store",
     }).then((res) => res.json())
     return data
