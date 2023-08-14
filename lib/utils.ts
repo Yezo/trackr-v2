a6f9c1b198add13e81dd9955bfecec8dcf097cad
Kev <sulpeum@gmail.com>
2023-08-14 15:52:30 -0400

    test: test fetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,8 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export async function getData(id: string | undefined) {
   try {
-    // const data = await fetch(`${process.env.LIVE_URL}/api/job/${id}`, {
-    const data = await fetch(`${process.env.LOCAL_URL}/api/job/${id}`, {
+    const data = await fetch(`${process.env.LIVE_URL}/api/job/${id}`, {
+      // const data = await fetch(`${process.env.LOCAL_URL}/api/job/${id}`, {
       cache: "no-store",
     }).then((res) => res.json())
     return data
