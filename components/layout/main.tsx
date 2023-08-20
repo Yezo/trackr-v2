06a1e0a767cec04ddc79a4b2f1b1fbb2d60514d0
Kev <sulpeum@gmail.com>
2023-08-20 19:09:48 -0400

    styles: edit css improvements

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -2,5 +2,7 @@ type Props = {
   children: React.ReactNode
 }
 export const MainContainer = ({ children }: Props) => {
-  return <main className="container mx-auto flex-1 space-y-8 px-4 py-8 md:px-32">{children}</main>
+  return (
+    <main className="container mx-auto flex-1 space-y-8 px-4 pb-24 pt-8 md:px-32">{children}</main>
+  )
 }
